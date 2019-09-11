0e1583f18a21b0ff256cc2b66483e8c0c0126732
Erick Benites Cuenca <erick.benites@gmail.com>
2019-09-11 16:23:32 -0500

    Initial commit

---
+++ app/src/main/java/pe/ebenites/cardidapp/MainActivity.java
@@ -0,0 +1,36 @@
+package pe.ebenites.cardidapp;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewQrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            String dni = "76523451";
+
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(dni,
+                    BarcodeFormat.CODE_128, 400, 160);
+            imageViewQrCode = findViewById(R.id.barcode_image);
+            imageViewQrCode.setImageBitmap(bitmap);
+
+        } catch(Exception e) {
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
